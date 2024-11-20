047b434775d32d587726e29f1bc42b4197ae32c8
Caleb Courier <caleb.courier@gmail.com>
2024-11-20 10:28:03 -0600

    fix fix_action; use lazy import

--- nemoguardrails/integrations/guardrails_ai/guard_actions.py
+++ nemoguardrails/integrations/guardrails_ai/guard_actions.py
@@ -2,10 +2,22 @@ from guardrails import Guard
 
 from nemoguardrails.rails.llm.llmrails import LLMRails
 
+try:
+    from guardrails import Guard
+except ImportError:
+    raise ImportError(
+        "Could not import guardrails-ai, please install it with "
+        "`pip install guardrails-ai`."
+    )
 
 def register_guardrails_guard_actions(rails: LLMRails, guard: Guard, guard_name: str):
     def fix_action(text, metadata={}):
-        return guard.validate(llm_output=text, metadata=metadata).validated_output
+        response = guard.validate(llm_output=text, metadata=metadata)
+        return (
+            response.validated_output
+            if response.validation_passed
+            else None
+        )
 
     def validate_action(text, metadata={}):
         return guard.validate(llm_output=text, metadata=metadata).validation_passed
